fe6223921a0adaf6e25c1761baf5a14f97cf0674
unknown <lolromik04@gmail.com>
2022-11-28 21:52:08 +0300

    add form

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from reviews.views import pelmeni
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',pelmeni,name="pelmeni"),
 ]
