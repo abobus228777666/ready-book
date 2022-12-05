a28b22e2ccc901b7146382c0cfa40f1de2f16d4e
unknown <lolromik04@gmail.com>
2022-12-05 21:56:59 +0300

    post form

--- reviews/views.py
+++ reviews/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from reviews.forms import ReviewForm
 
 def pelmeni(request):
-    return render(request,'pelmeni.html')
+    if request.method == 'GET':    
+        form = ReviewForm()
+        return render(request,'pelmeni.html',{'form':form})
+    elif request.method=='POST':
+        form=ReviewForm(request.POST)
+        if form.is_valid():
+            data=form.cleaned_data
+            print(data)
+    name=request.GET.get("aboba"),
+    bobamail=request.GET.get("gmailboba"),
+    review=request.GET.get("boba_review"),
+    return render(request,'pelmeni.html',{'aboba':name,'bobamail':bobamail,'boba_review':review,"form":form})
